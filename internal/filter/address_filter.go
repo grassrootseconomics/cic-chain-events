c73f35a21ea6ed2330b6952edd76a6190cf8b929
Mohammed Sohail <sohailsameja@gmail.com>
2023-09-06 16:16:23 +0800

    filer: add SRF voucher

--- internal/filter/address_filter.go
+++ internal/filter/address_filter.go
@@ -9,6 +9,7 @@ import (
 
 const (
 	KRNVoucherAddress = "0x8bab657c88eb3c724486d113e650d2c659aa23d2"
+	SRFVoucherAddress = "0x45d747172e77d55575c197cba9451bc2cd8f4958"
 )
 
 type (
@@ -28,7 +29,7 @@ func NewAddressFilter(o AddressFilterOpts) Filter {
 }
 
 func (f *AddressFilter) Execute(_ context.Context, transaction *fetch.Transaction) (bool, error) {
-	if transaction.To.Address == KRNVoucherAddress {
+	if transaction.To.Address == KRNVoucherAddress || transaction.To.Address == SRFVoucherAddress {
 		return true, nil
 	}
 
